4a0e28e4ae2168f9e6170a4af2634df1a2d6020d
meliGalvis <meli.galvis.montes@gmail.com>
2026-04-24 00:21:11 -0500

    servidro

--- Actividad4/backend/server.js
+++ Actividad4/backend/server.js
@@ -14,10 +14,11 @@ app.use(express.json());
 app.use("/api", require("./routes/usuarios"));
 app.use("/api", require("./routes/bovinos"));
 
+const PORT = process.env.PORT || 3000;
 
 // servidor
-app.listen(3000, () => {
-    console.log("🚀 Servidor corriendo en http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 
